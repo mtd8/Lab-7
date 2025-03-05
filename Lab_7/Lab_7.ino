ec05ba15b79f9a6a98bcae3e7c72ffdbbfd8fdd4
mtd8 <158385079+mtd8@users.noreply.github.com>
2025-03-05 14:10:48 -0600

    Imported BPM code from last week

--- Lab_7/Lab_7.ino
+++ Lab_7/Lab_7.ino
@@ -1,12 +1,60 @@
 #include "arduino_secrets.h"
 #include "thingProperties.h"
 
-void setup() {
-  // put your setup code here, to run once:
+const int sensor_pin = 0;
+int pulse_signal = 0;
+int pulse_period = 0;
+//float BPM = 0;
 
+//  initialize flags
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+int upper_threshold = 800;
+int lower_threshold = 200;
+
+void setup() {
+  // Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  delay(1500); 
+
+  // Defined in thingProperties.h
+  initProperties();
+
+  // Connect to Arduino IoT Cloud
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { 
+
+  ArduinoCloud.update();
+  pulse_signal = analogRead(sensor_pin);
+  //Serial.println(pulse_signal);
+  delay(100);
+  //Serial.println("first peak ");
+  //Serial.println(first_peak_detected);
+  //Serial.println("any peak ");
+  //Serial.println(any_peak_detected);
+  if (pulse_signal > upper_threshold && any_peak_detected == false) {
+    any_peak_detected = true;
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+      first_peak_detected = true;
+    }
+    else {
+      second_pulse_time = millis();
+      pulse_period = second_pulse_time - first_pulse_time;
+      bPM = 60000/pulse_period;
+      Serial.println(BPM);
+      first_peak_detected = false;
+    }
+  }
+  if (pulse_signal < lower_threshold) {
+    any_peak_detected = false;
+  }
 
 }
